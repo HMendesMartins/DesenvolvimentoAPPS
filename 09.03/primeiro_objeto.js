e8d7f8bcbe2cbcb108f25b10d77b924767474250
HMendesMartins <henriquemendesmartins9@gmail.com>
2026-03-09 10:57:08 -0300

    Changes

---
+++ 09.03/primeiro_objeto.js
@@ -0,0 +1,16 @@
+class Conta {
+    constructor (tipo, titular, saldo){
+        this.tipo = tipo;
+        this.titular = titular;
+        this.saldo = saldo;
+    };
+    mostrarExtrato(){
+        return `A conta ${this.tipo} do ${this.titular} tem ${this.saldo} de saldo.`
+    }
+    depositar(valor){
+        this.valor = this.valor + valor
+    };
+}
+let contaCorrente = new Conta("Corrente", "João", 1200)
+let contaPoupanca = new Conta("Pupança", "Maria", 2000)
+console.log(contaCorrente.mostrarExtrato());
