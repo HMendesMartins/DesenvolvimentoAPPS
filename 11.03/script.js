c6269dae9ef6337d82fc64073cc49b221ca455c0
HMendesMartins <henriquemendesmartins9@gmail.com>
2026-03-11 12:19:51 -0300

    App

--- 11.03/script.js
+++ 11.03/script.js
@@ -2,8 +2,15 @@ let botao = document.getElementById('botaoCor');
 botao.addEventListener('click',() => {
     let corAtual = 
       document.body.style.backgroundColor;
-      if(corAtual== 'fuchsia'){
-        document.body.style.backgroundColor = 'blue';
+      if(corAtual== 'yellow'){
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = "white";
+        botao.style.backgroundColor = "yellow";
+        botao.style.color = "black";
+        a
       } else  {
-        document.body.style.backgroundColor = 'fuchsia';     
+        document.body.style.backgroundColor = 'yellow';
+        document.body.style.color = "black";
+        botao.style.backgroundColor = "black";
+        botao.style.color = "white";
     }});
