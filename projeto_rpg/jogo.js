b95efdbefa08f98d4629c52a348c63cdfdd1f117
Rune <milaqc.bispo@gmail.com>
2026-03-25 12:12:05 -0300

    RPG

--- projeto_rpg/jogo.js
+++ projeto_rpg/jogo.js
@@ -1,4 +1,3 @@
-const { createElement } = require("react");
 
 class Personagem{
     constructor(nome, hp, mana, especial){
@@ -8,58 +7,89 @@ class Personagem{
         this.especial = especial;
     }
     atacar(alvo, habilidade){
-        this.alvo = alvo
-        this.habilidade = habilidade
-        if(this.mana >=habilidade.custo && this.energia >= habilidade.energia){
-            alvo.hp -= habilidade.dano;
-            if (habilidade.custo > 0){
-                this.mana -= habilidade.custo
-                this.energia += 50;
+        if(alvo.hp > 0 && this.hp > 0) {
+            if(this.mana >=habilidade.custo && this.especial >= habilidade.especial){
+                alvo.hp -= habilidade.dano;
+                console.log()
+                if (habilidade.custo > 0){
+                    this.mana -= habilidade.custo
+                    this.especial += 50;
+                }
+                else{
+                    this.mana += 20
+                }
+                if (habilidade.especial > 0){
+                    this.especial = 0
+                }
+                return `${this.nome} usou ${habilidade.nome}. Tirou ${habilidade.dano} de HP do inimigo!`
             }
-            if (habilidade.energia > 0){
-                this.energia = 0
+            else{
+                return `Sem recursos para usar essa habilidade!`;
             }
-            msg = `${this.nome} usou ${habilidade.nome}. Tirou ${habilidade.dano} de HP do inimigo!`
         }
-        else{
-            msg = `Sem recursos para usar essa habilidade!`
+        if(alvo.hp <= 0){
+            alert(`${alvo.nome} MORREU! Pressione F5 para recomeçar`)
         }
-        
-        return(msg)
+        if(this.hp <= 0){
+            alert(`${this.nome} MORREU! Pressione F5 para recomeçar`)
+        }
+    }
+    boss_atacar(alvo){
+        if(alvo.hp > 0 && this.hp > 0){
+            if(this.especial == 100){
+                alvo.hp -= 30
+                this.especial = 0
+            }
+            else{
+                this.especial +=50
+                alvo.hp -= 15
+            }
+        }
+        if(alvo.hp <= 0){
+            alert(`${alvo.nome} MORREU! Pressione F5 para recomeçar`)
+        }
+        if(this.hp <= 0){
+            alert(`${this.nome} MORREU! Pressione F5 para recomeçar`)
+        }
     }
 }
-const atualizarInterface = (mensagem) =>{
+
+const atualizarInterface = (mensagem) => {
+    console.log(boss.hp)
     document.getElementById("hp-heroi").value = hero.hp;
     document.getElementById("mp-heroi").value = hero.mana;
     document.getElementById("especial-heroi").value = hero.especial;
+    document.getElementById("hp-boss").value = boss.hp;
+    document.getElementById("mp-boss").value = boss.mana;
+    document.getElementById("especial-boss").value = boss.especial;
 }
 class Habilidade{
-    constructor(id, nome, dano, custo, energia){
+    constructor(id, nome, dano, custo, especial){
         this.id = id
         this.nome = nome
         this.dano = dano
         this.custo = custo
-        this.energia = energia
+        this.especial = especial
     }
 }
-let hero = new Personagem("Carvagon, O Santo Padroeiro Da Identidade Canina", 200, 100, 0);
-document.getElementById("nome-heroi").textContent = `${hero.nome}`;
-let boss = new Personagem("Pivolo, o Mau Dono do Mausoléu!", 1000, 100, 0)
-document.getElementById("nome-boss").textContent = `${boss.nome}`
+let hero = new Personagem("Carvagon, O Santo Padroeiro Da Identidade Canina", 100, 100, 0);
+
+let boss = new Personagem("Pivolo, o Mau Dono do Mausoléu!", 500, 100, 0)
 let listaHabilidades = [
     new Habilidade(1, "Ataque Básico", 20, 0, 0),
-    new Habilidade(2, "Ataque Skill", 20, 20, 0),
-    new Habilidade(3, "Ataque Especial", 20, 0, 100)
+    new Habilidade(2, "Ataque Skill", 80, 20, 0),
+    new Habilidade(3, "Ataque Especial", 150, 0, 100)
 ]
+let container = document.getElementById("controles")
 listaHabilidades.forEach(hab =>{
     let btn = document.createElement("button")
     btn.innerText = hab.nome
     btn.classList.add("btn","btn-warning")
-    container.appendChiId(btn)
+    container.appendChild(btn)
     btn.onclick = () => {
         let mensagem = hero.atacar(boss, hab)
         atualizarInterface(mensagem)
-        boss.atacar(hero, hab)
+        boss.boss_atacar(hero)
     }
 })
 
