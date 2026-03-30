e777d4fa841b24afa4f12699541086999b292c48
Rune <milaqc.bispo@gmail.com>
2026-03-30 12:21:05 -0300

    react

---
+++ app02/helloWorld/app/index.tsx
@@ -0,0 +1,18 @@
+import {Text, View, StyleSheet} from 'react-native'
+
+export default function Index() {
+    return(
+        <View style = {estilo.container}>
+            <Text>
+                Hello, world!
+            </Text>
+        </View>
+    )
+}
+const estilo = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
